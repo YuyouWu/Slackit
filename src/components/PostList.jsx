0e0086e24380789988865a3f3d132f00ce619cb3
YuyouWu <yuyouwu5@gmail.com>
2020-04-18 21:05:27 -0700

    Added single post view

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import { Item } from 'semantic-ui-react';
+import { Item, Grid } from 'semantic-ui-react';
 import axios from 'axios';
 import slackbot from './../img/slackbot.png'
+
+import PostView from './PostView';
 
 class PostList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             currentSub: props.currentSub || 'all',
-            postsData: []
+            postsData: [],
+            title: '',
+            author: '',
+            showPost: false
         }
     }
 
-    getPostData = () => {
+    getPostList = () => {
         axios.get(`https://old.reddit.com/r/${this.state.currentSub}.json`).then((res) => {
             this.setState({
-                postsData: res.data.data.children,
+                postsData: res.data.data.children
             })
         });
     }
 
+    closePostView = () => {
+        this.setState({
+            showPost: false
+        });
+    }
+
     componentDidMount() {
-        this.getPostData();
+        this.getPostList();
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
@@ -34,85 +45,84 @@ class PostList extends React.Component {
 
     componentDidUpdate(prevProps) {
         if (prevProps.currentSub !== this.state.currentSub) {
-            this.getPostData();
+            this.getPostList();
         }
     }
 
     render() {
         return (
-            <div style={{ height: '100vh', paddingTop: 20, overflow: 'auto' }}>
-                {/* <Item.Group>
-                    <Item
-                        style={{
-                            paddingLeft: 20,
-                            position: "absolute",
-                        }}
-                    >
-                        <Item.Content>
-                            <Item.Meta
-                                style={{
-                                    fontWeight: 'bold',
-                                    color: 'black'
-                                }}
-                            >
-                                # {this.state.currentSub}
-                            </Item.Meta>
-                        </Item.Content>
-                    </Item>
-                </Item.Group> */}
-                <Item.Group style={{ paddingTop: 10 }}>
-                    {this.state.postsData ?
-                        this.state.postsData.map((post, i) => {
-                            const author = post.data.author;
-                            const title = post.data.title
-                            return (
-                                <Item
-                                    key={i}
-                                    className="postTile"
-                                    style={{
-                                        paddingLeft: 20
-                                    }}
-                                    onClick={() => {
-                                        console.log(title);
-                                    }}
-                                >
-                                    <Item.Image
-                                        style={{
-                                            height: 35,
-                                            width: 35,
-                                            marginTop: 5,
-                                            borderRadius: '10%',
-                                            overflow: 'hidden'
-                                        }}
-                                        src={slackbot}
-                                    />
-                                    <Item.Content>
-                                        <Item.Meta
-                                            style={{
-                                                fontWeight: 'bold',
-                                                color: 'black'
-                                            }}
-                                        >
-                                            {author}
-                                        </Item.Meta>
-                                        <Item.Meta
-                                            style={{
-                                                color: 'black'
-                                            }}
-                                        >
-                                            {title}
-                                        </Item.Meta>
-                                    </Item.Content>
-                                </Item>
-
-                            )
-                        })
-                        : (
-                            <p> You done goofed </p>
-                        )
+            <Grid>
+                <Grid.Row style={{ height: '100vh' }}>
+                    <Grid.Column width={this.state.showPost ? 10 : 16}>
+                        <div style={{ height: '100vh', paddingTop: 20, overflow: 'auto' }}>
+                            <Item.Group style={{ paddingTop: 10 }}>
+                                {this.state.postsData ?
+                                    this.state.postsData.map((post, i) => {
+                                        const author = post.data.author;
+                                        const title = post.data.title
+                                        const permaLink = post.data.permalink;
+                                        return (
+                                            <Item
+                                                key={i}
+                                                className="postTile"
+                                                style={{
+                                                    paddingLeft: 20
+                                                }}
+                                                onClick={() => {
+                                                    this.setState({
+                                                        permaLink: permaLink,
+                                                        title: title,
+                                                        author: author,
+                                                        showPost: true
+                                                    });
+                                                }}
+                                            >
+                                                <Item.Image
+                                                    style={{
+                                                        height: 35,
+                                                        width: 35,
+                                                        marginTop: 5,
+                                                        borderRadius: '10%',
+                                                        overflow: 'hidden'
+                                                    }}
+                                                    src={slackbot}
+                                                />
+                                                <Item.Content>
+                                                    <Item.Meta
+                                                        style={{
+                                                            fontWeight: 'bold',
+                                                            color: 'black'
+                                                        }}
+                                                    >
+                                                        {author}
+                                                    </Item.Meta>
+                                                    <Item.Meta
+                                                        style={{
+                                                            color: 'black'
+                                                        }}
+                                                    >
+                                                        {title}
+                                                    </Item.Meta>
+                                                </Item.Content>
+                                            </Item>
+                                        )
+                                    })
+                                    : (
+                                        <p> You done goofed </p>
+                                    )
+                                }
+                            </Item.Group>
+                        </div>
+                    </Grid.Column>
+                    {this.state.showPost &&
+                        <Grid.Column width={6}>
+                            <div style={{ height: '100vh', paddingTop: 20, overflow: 'auto' }}>
+                                <PostView closePostView={this.closePostView} permaLink={this.state.permaLink} title={this.state.title} author={this.state.author}/>
+                            </div>
+                        </Grid.Column>
                     }
-                </Item.Group>
-            </div>
+                </Grid.Row>
+            </Grid>
         )
     }
 }
---
+++ src/components/PostView.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Button, Item, Image } from 'semantic-ui-react';
+import axios from 'axios';
+import slackbot from './../img/slackbot.png'
+
+class PostView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            postData: '',
+            commentData: '',
+            title: props.title
+        }
+    }
+
+    getPostData = () => {
+        axios.get(`https://www.reddit.com${this.props.permaLink}.json`).then(res => {
+            this.setState({
+                postData: res.data[0].data.children[0].data,
+                commentData: res.data[1].data.children
+            })
+        })
+    }
+
+    renderPostImage = () => {
+        return (
+            <div style={{margin: 10}}>
+                <Image
+                    fluid
+                    src={this.state.postData.url}
+                />
+            </div>
+        )
+    }
+
+
+    componentDidMount() {
+        this.getPostData();
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.title !== prevState.title) {
+            return ({
+                title: nextProps.title
+            })
+        } else return null;
+    }
+
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.title !== this.state.title) {
+            this.getPostData();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Button
+                    basic
+                    onClick={() => {
+                        this.props.closePostView()
+                    }}
+                >
+                    X
+                </Button>
+                <Item.Group>
+                    <Item>
+                        <Item.Image
+                            style={{
+                                height: 35,
+                                width: 35,
+                                marginTop: 5,
+                                borderRadius: '10%',
+                                overflow: 'hidden'
+                            }}
+                            src={slackbot}
+                        />
+                        <Item.Content>
+                            <Item.Meta
+                                style={{
+                                    fontWeight: 'bold',
+                                    color: 'black'
+                                }}
+                            >
+                                {this.props.author}
+                            </Item.Meta>
+                            <Item.Meta
+                                style={{
+                                    color: 'black'
+                                }}
+                            >
+                                {this.props.title}
+                            </Item.Meta>
+                            {this.renderPostImage()}
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </div>
+        )
+    }
+}
+export default PostView
